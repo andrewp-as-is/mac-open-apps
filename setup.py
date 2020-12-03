80a871205fe3d23a7aca24c610b6b8aeb0b84e0f
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-04 00:07:39 +0300

    2020:12:04 00:07

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='mac-open-apps',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages(),
     scripts=['scripts/open-apps']
